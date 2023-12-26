1db20b945c13ec922ea86869b7c94017730f512d
FerndWehbe <51804490+FerndWehbe@users.noreply.github.com>
2023-12-26 02:24:20 -0300

    Correção do docker-compsoe e alterado config para pegar dados do banco de dados no docker

--- app/config/config.py
+++ app/config/config.py
@@ -1,9 +1,9 @@
-# config.py
+import os
 
-POSTGRES_USER = ''
-POSTGRES_PASSWORD = ''
-POSTGRES_DB = ''
-POSTGRES_HOST = 'localhost'
-POSTGRES_PORT = '5432'
+POSTGRES_DB = os.environ.get("POSTGRES_DB", "app")
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST", "localhost")
+POSTGRES_USER = os.environ.get("POSTGRES_USER", "postgres")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "postgres")
+POSTGRES_PORT = os.environ.get("POSTGRES_PORT", 5432)
 
-DATABASE_URL = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"  # noqa
