91dd20fe481b2f9251ac605ba8e17388f9110d56
FerndWehbe <51804490+FerndWehbe@users.noreply.github.com>
2023-12-29 18:28:58 -0300

    Adicionado configuração de cors para o front-end

--- app/main.py
+++ app/main.py
@@ -1,11 +1,25 @@
 from celery.result import AsyncResult
 from database import Base, engine
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from models import Match, Player, PlayerMatchAssociation
 from repository import get_player_by_name
 from tasks import get_summoner_info
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(
     bind=engine,
