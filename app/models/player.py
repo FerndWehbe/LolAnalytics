61a2800c74662badd14f5540bde005c097b7c965
FerndWehbe <51804490+FerndWehbe@users.noreply.github.com>
2023-12-30 17:06:51 -0300

    Adicionado coluna de região à tabela player

--- app/models/player.py
+++ app/models/player.py
@@ -8,7 +8,18 @@ class Player(Base):
     puuid = Column(String, primary_key=True, unique=True)
     name = Column(String)
     riot_id = Column(String)
+    region = Column(String)
     update = Column(DateTime)
     rewind_id = Column(DateTime)
 
     matches = relationship("PlayerMatchAssociation", back_populates="player")
+
+    def to_dict(self):
+        return {
+            "puuid": self.puuid,
+            "name": self.name,
+            "region": self.region,
+            "riot_id": self.riot_id,
+            "update": self.update,
+            "rewind_id": self.rewind_id,
+        }
