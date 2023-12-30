4b0daf10dd4aff2bcd01a4e4392bc9aee7dd6e7e
FerndWehbe <51804490+FerndWehbe@users.noreply.github.com>
2023-12-30 01:17:59 -0300

    Alteração na tabela de player_match_association para que não exista a dupla partida e player duplicado.

--- app/models/player_match_association.py
+++ app/models/player_match_association.py
@@ -1,14 +1,13 @@
 from database import Base
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.orm import relationship
 
 
 class PlayerMatchAssociation(Base):
     __tablename__ = "PlayerMatchAssociation"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    player_puuid = Column(String, ForeignKey("Player.puuid"))
-    match_id = Column(String, ForeignKey("Match.match_id"))
+    player_puuid = Column(String, ForeignKey("Player.puuid"), primary_key=True)
+    match_id = Column(String, ForeignKey("Match.match_id"), primary_key=True)
 
     player = relationship("Player", back_populates="matches")
     match = relationship("Match", back_populates="players")
