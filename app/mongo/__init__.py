94d2d268d5edaa29968c31bf87aea482ec29803b
FerndWehbe <51804490+FerndWehbe@users.noreply.github.com>
2023-12-30 13:03:32 -0300

    Correção do import do metodo de insert_many_matches_data

--- app/mongo/__init__.py
+++ app/mongo/__init__.py
@@ -1,5 +1,5 @@
 from .operations import find_match_by_id  # noqa
 from .operations import find_matches_by_ids  # noqa
 from .operations import find_matches_by_puuid  # noqa
-from .operations import inser_many_matches_data  # noqa
+from .operations import insert_many_matches_data  # noqa
 from .operations import insert_match_data  # noqa
