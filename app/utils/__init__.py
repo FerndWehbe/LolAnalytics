a6fbd1a6ed1ac49ef2f3706354fe6498d80609fe
8b1tz <yohannaoliveira12@gmail.com>
2023-12-31 01:53:27 -0300

    Colocando funções para rota de estatisticas

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,2 +1,5 @@
+from .functions import (calculate_baron_takedowns,  # noqa
+                        calculate_participant_totals,
+                        extract_participant_items, get_champions_lost_most)
 from .rate_limit_control import RateLimiter  # noqa
 from .time_calculator import get_timestamp_from_year  # noqa
